57042fde9163c9118ed0dcdd204f5727b469dc68
Sotaro KARASAWA <sotarok@crocos.co.jp>
2011-08-23 00:25:44 +0900

    misc: local git-daily generator add symlink to your bin dir

--- src/bin/.gen-local-git-daily.sh
+++ src/bin/.gen-local-git-daily.sh
@@ -2,5 +2,17 @@
 
 BASE_DIR=$(cd $(dirname $0); pwd)
 LOCAL_BIN_FILE=$BASE_DIR/git-daily-local
-cat $BASE_DIR/git-daily | sed -e 's/@php_bin@/'$(which php | sed -e 's/\//\\\//g')'/' > $LOCAL_BIN_FILE
+HOME_BIN_FILE=$HOME/bin/git-daily
+
+cat $BASE_DIR/git-daily | sed -e 's/@php_bin@/'$(which php | sed -e 's/\//\\\//g')'/' > $LOCAL_BIN_FILE && echo "Generated: $LOCAL_BIN_FILE"
 chmod +x $LOCAL_BIN_FILE
+
+# set
+
+for p in $(echo $PATH | sed -s 's/:/ /g' )
+do
+    if [ "$p" = "$HOME/bin" ]
+    then
+        ln -snf $LOCAL_BIN_FILE $HOME_BIN_FILE && echo "Symlinked: $HOME_BIN_FILE -> $LOCAL_BIN_FILE"
+    fi
+done
